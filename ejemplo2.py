e7a44dfb583cd45cc646d06f3207935222398529
Mvelez <velez.matias95@gmail.com>
2024-07-01 20:51:42 -0300

    mi 3era version

---
+++ ejemplo2.py
@@ -0,0 +1,9 @@
+def msj():
+    print("estoy trabajando con git")
+    
+msj()
+
+def sumar(n1,n2):
+    print("el resultado de la suma es: ",n1+n2)
+    
+sumar(int(input("Ingrese el primer numero")),int(input("Ingrese el segundo numero")))
