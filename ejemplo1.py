c6ed2a0e1bcb99385f7ce2dcab5c8fa0a83f3883
Mvelez <velez.matias95@gmail.com>
2024-07-01 21:01:49 -0300

    AGREGO DIVICION Y MULTIPLICAR

--- ejemplo1.py
+++ ejemplo1.py
@@ -12,3 +12,8 @@ def restar(n1,n2):
     print("el resultado de la resta es: ",n1-n2)
     
 restar(int(input("Ingrese el primer numero")),int(input("Ingrese el segundo numero")))
+
+def multiplicar(n1,n2):
+    print("el resultado de la multiplicacion es: ",n1*n2)
+    
+multiplicar(int(input("Ingrese el primer numero")),int(input("Ingrese el segundo numero")))
--- ejemplo2.py
+++ ejemplo2.py
@@ -8,3 +8,7 @@ def restar(n1,n2):
     
 restar(int(input("Ingrese el primer numero")),int(input("Ingrese el segundo numero")))
 
+def divic(n1,n2):
+    print("el resultado de la DIVICION es: ",n1/n2)
+    
+divic(int(input("Ingrese el primer numero")),int(input("Ingrese el segundo numero")))
